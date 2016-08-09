d9fa08d3ca1606fdc8e0cdf5707dd0d45b4893e3
Darkspine77 <Jwoods@Riverdale.edu>
2016-08-09 13:18:02 -0400

    removed usless alerts

--- js/index.js
+++ js/index.js
@@ -45,7 +45,6 @@
           'Username': Username,
           'Password': Password
         });
-        alert("Account created under username: " + Username)
       }
 
       function login(){
@@ -66,46 +65,9 @@
         }
         
         if(passCheck = Password && passCheck != null){
-          alert("Welcome back " + Username);
           document.location.href = "feed.html";
         }
-    }
-
-
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 20
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-            map
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
+    } 
 
           function nxtImg(){
       $(body).css("background-image",'url('+newURL);
