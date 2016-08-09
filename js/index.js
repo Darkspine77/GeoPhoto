c99d13ddf31532cf58988dbe944c0ace7ba662fb
Darkspine77 <Jwoods@Riverdale.edu>
2016-08-09 10:25:37 -0400

    typeo #2

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@
         var profile = googleUser.getBasicProfile();
         var id_token = googleUser.getAuthResponse().id_token;
         $("#username").val(profile.getGivenName());
-        $("#password").val(profile.getID());
+        $("#password").val(profile.getId());
         firebase.database().ref('users/' + $("#username").val()).on('value', function(snapshot) {
           if(snapshot.val() == null){
               signup();
