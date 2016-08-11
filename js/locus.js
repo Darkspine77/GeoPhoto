d3ec011f14b85d27e324d1dc810e6710e45f7768
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-11 15:50:38 -0400

    typo

--- js/locus.js
+++ js/locus.js
@@ -40,7 +40,7 @@ navigator.geolocation.getCurrentPosition(function(position){
         geocoder.geocode({'location': latlng}, function(results, status) {
           if (status === 'OK') {
             var locinfo = results[1].formatted_address.split(',')
-            area = locinfo[0]
+            area = locinfo[0];
             console.log("asdsads");
             console.log(area);
         }
