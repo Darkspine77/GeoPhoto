6ebffd94fa3898f6e2daf2ad764a2ec5f4b04963
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-11 13:15:08 -0400

    s

--- js/locus.js
+++ js/locus.js
@@ -72,7 +72,6 @@ var click = false;
 
 function upload() {
     var name = account.User;
-    var like = 0;
     if($('#file2').val() != "" || geo != ""){
         var file = document.getElementById("file2").files[0];
         // We can use the 'name' property on the File API to get our file name
@@ -86,7 +85,7 @@ function upload() {
                 'locus': area,
                 'coords': coords,
                 'image': img,
-                'like': like
+                'like': 0
             });
         });
     $('.upload').animate({
