61927b4188ac300241d6c6aedfe3980bffecc8f6
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-11 09:35:04 -0400

    z

--- js/locus.js
+++ js/locus.js
@@ -95,6 +95,7 @@ function upload() {
 database.on('child_added',function(dataRow){
 	//getting raw values
   	var row = dataRow.val();
+    console.log(dataRow);
   	//adding to the div
     withinLat = row.coords[0] < (lat + .00723) && row.coords[0] > (lat - .00723);
     withinLon = row.coords[1] < (lon + .00723) && row.coords[1] > (lon - .00723);
