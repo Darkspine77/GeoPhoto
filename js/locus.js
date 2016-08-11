40c3d360f16f1117ca560962be109fac011f8937
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-11 13:18:57 -0400

    asd

--- js/locus.js
+++ js/locus.js
@@ -114,7 +114,7 @@ database.on('child_added',function(dataRow){
     if(withinLon && withinLat){
         $(".locus").append(
             '<div class="photo"><div class="info"><h2 class="user">' + row.name + '|' + row.locus +
-            '</h2><button type="button" name="button" class="button" onclick="likeme(' + string + ')">like</button><h2 class="likes">' + row.like +
+            '</h2><button type="button" name="button" class="button" onclick="likeme(' + dataRow.key + ')">like</button><h2 class="likes">' + row.like +
             '</h2></div><div class="center"><img src="' + row.image + '" class="width"/></div></div>'
         );
     }
