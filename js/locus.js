7dff1f9b0ac4c01e0d7c48c22986ade8c65b7b8c
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-11 15:56:03 -0400

    dsc

--- js/locus.js
+++ js/locus.js
@@ -84,7 +84,7 @@ var click = false;
 function upload() {
     var name = account.User;
     var like = 0;
-    if($('#file2').val() != "" || geo != ""){
+    if($('#file2').val() != ""){
         var file = document.getElementById("file2").files[0];
         // We can use the 'name' property on the File API to get our file name
         var uploadTask = storageRef.child('images/' + file.name).put(file);
