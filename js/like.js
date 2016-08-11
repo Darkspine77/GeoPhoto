9d9d4a5bc263ba0503cabc89335a52047abc7ac9
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-11 12:05:37 -0400

    j

--- js/like.js
+++ js/like.js
@@ -1,5 +1,6 @@
 function likeme(id) {
     var like = firebase.database().ref('images/-' + id + '/like').val()
+    console.log(like);
     firebase.database().ref('images/-' + id).set({
         'like': like + 1
     })
