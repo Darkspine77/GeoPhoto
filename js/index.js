2d56c620bc700f6085d8aef72d1e36b2ba206a5c
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-15 09:56:42 -0400

    hih

--- js/index.js
+++ js/index.js
@@ -78,13 +78,11 @@ var counter = 0;
 
 var pleasework = setInterval(function() {
     function delay(){
-        $("#div1").css("background-image",'url(' + stuff[counter] + ')');
+        $("#body").css("background-image",'url(' + stuff[counter] + ')');
         }
         if (counter > (stuff.length - 1) ) {
         counter = 0;
         }
-$("#div1").fadeOut(1500);
-$("#div1").fadeIn(1500);
 setTimeout(delay,1500)
 counter++;
 }, 5000);
