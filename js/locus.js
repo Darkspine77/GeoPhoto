e8970dca0a63694d8436b7ec903371d5cdab98b4
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-15 21:39:42 -0400

    jnbob

--- js/locus.js
+++ js/locus.js
@@ -181,7 +181,7 @@ function clean() {
             });
         }
     });
-    firebase.database().ref('/images').onbe('value').then(function(a) {
+    firebase.database().ref('/images').on('value', function(a) {
         $('.locus').text('');
         var b = a.val();
         for (k in b) {
@@ -222,7 +222,7 @@ $(document).ready(function() {
             });
         }
     });
-    firebase.database().ref('/images').on('value', function(a) {
+    firebase.database().ref('/images').once('value').then(function(a) {
         $('.locus').text('');
         var b = a.val();
         for (k in b) {
