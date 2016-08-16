5982cd5efe2395a4e74121c20c5281016bf70f4f
Ibrahim Diallo <idial2242@inwoodearlycollege.org>
2016-08-15 21:11:36 -0400

    ds

--- js/locus.js
+++ js/locus.js
@@ -42,26 +42,6 @@ navigator.geolocation.getCurrentPosition(function(position){
 var storageRef = firebase.storage().ref();
 var click = false;
 
-firebase.database().ref('/images').on('child_changed', function(asdf) {
-    $('.locus').text('');
-    var b = asdf.val();
-    for (k in b) {
-        firebase.database().ref('/images/' + k).on('value', function(d) {
-            var c = d.val();
-            withinLat = c.coords[0] < (lat + .00723) && c.coords[0] > (lat - .00723);
-            withinLon = c.coords[1] < (lon + .00723) && c.coords[1] > (lon - .00723);
-            if(withinLat && withinLon) {
-                $(".locus").prepend(
-                    '<div id="' + d.key + '" class="photo"><div class="info"><h2 class="user">' + c.name + '|' + c.locus +
-                    '</h2><button type="button" name="button" class="button" onclick="likeme(' + "'" + d.key + "'" +
-                    ')">like</button><h2 class="likes">' + c.like +
-                    '</h2></div><div class="center"><img src="' + c.image + '" class="width"/></div></div>'
-    	        );
-            }
-    	})
-    }
-});
-
 function upload() {
     coords = [];
     navigator.geolocation.getCurrentPosition(function(position){
